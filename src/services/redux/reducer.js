77157de80f2c17f896ad08bfcf429b0a0101a1dc
diegosuarez-dev <dsuarezleon@gmail.com>
2020-10-08 17:18:12 +0100

    Creación del componente Top10 y el initialState

--- src/services/redux/reducer.js
+++ src/services/redux/reducer.js
@@ -1,9 +1,15 @@
 const initialState = {
-
+    movies: [],
+    series: []
 }
 
 function reducer (state = initialState, action) {
+    switch (action.type) {
 
+        
+        default:
+            return state;
+    }
 }
 
 export default reducer;
