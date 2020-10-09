0ec4efd15be708797f9364d7006d8dd0031ce8d4
diegosuarez-dev <dsuarezleon@gmail.com>
2020-10-09 17:50:14 +0100

    Implementado el top10

--- src/components/Top10/Top10.jsx
+++ src/components/Top10/Top10.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import axios from "axios";
 import { loadTop10Action } from '../../services/redux/actions';
+import './Top10.scss';
 
 const Top10 = props => {
     useEffect(
         () => {
-            props.loadTop10(props.match.params.itemType);
+            //eslint-disable-next-line
+            axios.get(`https://api.themoviedb.org/3/${props.match.params.itemType}/popular?api_key=9b4d066eedd374b20d0be2192a5327ec&language=es-ES&page=1`).then(res => props.loadTop10(props.match.params.itemType, res.data.results)).catch(console.error);            
         }
+        //eslint-disable-next-line
         ,[props.match.params.itemType]);
     if (props.match.params.itemType === 'movie') {
         console.log(props.movies);
@@ -19,13 +23,13 @@ const Top10 = props => {
                             <div className="media" key={movie.id}>
                                 <img src={"https://image.tmdb.org/t/p/w200" + movie.poster_path} className="mr-3" alt={`Poster of ${movie.title}`}></img>
                                 <div className="media-body">
-                                    <h5 className="mt-0">{movie.title}</h5>
+                                    <h4 className="mt-0">{movie.title}</h4>
                                     <h6 className="mt-0">Título original: {movie.original_title}</h6>
                                     <h6 className="mt-0">Fecha de lanzamiento: {movie.release_date}</h6>
                                     <h6 className="mt-0">Género: {}</h6>
                                     <h6 className="mt-0">Número de votos: {movie.vote_count}</h6>
                                     <h6 className="mt-0">Puntuación media: {movie.vote_average}</h6>
-                                    {movie.overview}
+                                    <p>{movie.overview}</p>
                                 </div>
                             </div>
                         )
--- src/services/redux/actions.js
+++ src/services/redux/actions.js
@@ -1,15 +1,6 @@
-import axios from "axios";
-
-const get10MorePopular = (itemType) => {
-    let items = [];
-    console.log(itemType);
-    axios.get(`https://api.themoviedb.org/3/${itemType}/popular?api_key=9b4d066eedd374b20d0be2192a5327ec&language=es-ES&page=1`).then(res => res.data.results.map((item) => items.push(item))).catch(console.error);
-    return items;
-}
-
-export const loadTop10Action = (dispatch) => (itemType) => 
+export const loadTop10Action = (dispatch) => (itemType, items) => 
     dispatch({
         type: 'LOAD_TOP10',
         itemType: itemType,
-        items: get10MorePopular(itemType)
+        items: items
     });
