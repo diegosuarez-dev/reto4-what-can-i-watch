97c60bea4184409bf2afb5602822cfc76b74c0e1
diegosuarez-dev <dsuarezleon@gmail.com>
2020-10-11 13:59:41 +0100

    Creación del componente DetailedMovie y preparación de la estructura para acceder al detalle de cada película o serie

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Movie = props => {
 
     return (
-        <div className="media" key={props.movie.id}>
-            <img src={"https://image.tmdb.org/t/p/w200" + props.movie.poster_path} className="mr-3" alt={`Poster of ${props.movie.title}`}></img>
-            <div className="media-body">
-                <h4 className="mt-0">{props.movie.title}</h4>
-                <h6 className="mt-0">Título original: {props.movie.original_title}</h6>
-                <h6 className="mt-0">Fecha de lanzamiento: {props.movie.release_date}</h6>
-                <h6 className="mt-0">Géneros:</h6>
-                <ul>{props.movie.genre_ids.map(
-                    gId => (
-                        <li key={props.movie.id + gId}>
-                            {props.genres.filter(mGen => mGen.id === gId)[0].name}
-                        </li>
-                    )
-                )}</ul>
-                <h6 className="mt-0">Número de votos: {props.movie.vote_count}</h6>
-                <h6 className="mt-0">Puntuación media: {props.movie.vote_average}</h6>
-                <p>{
-                    props.movie.overview.length > 140
-                        ? props.movie.overview.substring(0, props.movie.overview.indexOf(" ",139)) + '... '
-                        : props.movie.overview
-                }</p>
+        <Link to={`/movie/${props.movie.id}`}> 
+            <div className="media" key={props.movie.id}>
+                <img src={"https://image.tmdb.org/t/p/w200" + props.movie.poster_path} className="mr-3" alt={`Poster of ${props.movie.title}`}></img>
+                <div className="media-body">
+                    <h4 className="mt-0">{props.movie.title}</h4>
+                    <h6 className="mt-0">Título original: {props.movie.original_title}</h6>
+                    <h6 className="mt-0">Fecha de lanzamiento: {props.movie.release_date}</h6>
+                    <h6 className="mt-0">Géneros:</h6>
+                    <ul>{props.movie.genre_ids.map(
+                        gId => (
+                            <li key={props.movie.id + gId}>
+                                {props.genres.filter(mGen => mGen.id === gId)[0].name}
+                            </li>
+                        )
+                    )}</ul>
+                    <h6 className="mt-0">Número de votos: {props.movie.vote_count}</h6>
+                    <h6 className="mt-0">Puntuación media: {props.movie.vote_average}</h6>
+                    <p>{
+                        props.movie.overview.length > 140
+                            ? props.movie.overview.substring(0, props.movie.overview.indexOf(" ", 139)) + '... '
+                            : props.movie.overview
+                    }</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
--- src/components/Series/Series.jsx
+++ src/components/Series/Series.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Series = props => {
 
     return (
-        <div className="media" key={props.serie.id}>
-            <img src={"https://image.tmdb.org/t/p/w200" + props.serie.poster_path} className="mr-3" alt={`Poster of ${props.serie.name}`}></img>
-            <div className="media-body">
-                <h5 className="mt-0">{props.serie.name}</h5>
-                <h6 className="mt-0">Título original: {props.serie.original_name}</h6>
-                <h6 className="mt-0">Primera emisión: {props.serie.first_air_date}</h6>
-                <h6 className="mt-0">Géneros:</h6>
-                <ul>{props.serie.genre_ids.map(
-                    sgId => (
-                        <li key={props.serie.id + sgId}>
-                            {props.genres.filter(sGen => sGen.id === sgId)[0].name}
-                        </li>
-                    )
-                )}</ul>
-                <h6 className="mt-0">Número de votos: {props.serie.vote_count}</h6>
-                <h6 className="mt-0">Puntuación media: {props.serie.vote_average}</h6>
-                <p>{
-                    props.serie.overview.length > 140
-                        ? props.serie.overview.substring(0, props.serie.overview.indexOf(" ",139)) + '... '
-                        : props.serie.overview
-                }</p>
+        <Link to={`/tv/${props.serie.id}`}>
+            <div className="media" key={props.serie.id}>
+                <img src={"https://image.tmdb.org/t/p/w200" + props.serie.poster_path} className="mr-3" alt={`Poster of ${props.serie.name}`}></img>
+                <div className="media-body">
+                    <h5 className="mt-0">{props.serie.name}</h5>
+                    <h6 className="mt-0">Título original: {props.serie.original_name}</h6>
+                    <h6 className="mt-0">Primera emisión: {props.serie.first_air_date}</h6>
+                    <h6 className="mt-0">Géneros:</h6>
+                    <ul>{props.serie.genre_ids.map(
+                        sgId => (
+                            <li key={props.serie.id + sgId}>
+                                {props.genres.filter(sGen => sGen.id === sgId)[0].name}
+                            </li>
+                        )
+                    )}</ul>
+                    <h6 className="mt-0">Número de votos: {props.serie.vote_count}</h6>
+                    <h6 className="mt-0">Puntuación media: {props.serie.vote_average}</h6>
+                    <p>{
+                        props.serie.overview.length > 140
+                            ? props.serie.overview.substring(0, props.serie.overview.indexOf(" ", 139)) + '... '
+                            : props.serie.overview
+                    }</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
