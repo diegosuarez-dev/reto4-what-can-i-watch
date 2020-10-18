18da06e5b340d3913aa5d56714bceaea309086cc
diegosuarez-dev <dsuarezleon@gmail.com>
2020-10-18 17:56:10 +0100

    Updates

---
+++ precache-manifest.572130ec9fe930a03f6446ae59b307e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce48b529f17be8399af0465dde4932e",
+    "url": "/reto4-what-can-i-watch/index.html"
+  },
+  {
+    "revision": "6d1206ca234d0ea06e36",
+    "url": "/reto4-what-can-i-watch/static/css/main.6856c631.chunk.css"
+  },
+  {
+    "revision": "82a506d0394730487e2a",
+    "url": "/reto4-what-can-i-watch/static/js/2.4229f8a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reto4-what-can-i-watch/static/js/2.4229f8a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1206ca234d0ea06e36",
+    "url": "/reto4-what-can-i-watch/static/js/main.14fb8603.chunk.js"
+  },
+  {
+    "revision": "aa5f3e43172e81ed749f",
+    "url": "/reto4-what-can-i-watch/static/js/runtime-main.32b07226.js"
+  },
+  {
+    "revision": "86ff7c17f7e92af5da77274697bbff25",
+    "url": "/reto4-what-can-i-watch/static/media/blue_long_1.86ff7c17.svg"
+  }
+]);
